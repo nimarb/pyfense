ff2710a08908c9ae603811c8742805ed9d7ed583
Nimar <l.nimar.b@gmail.com>
2015-05-19 18:01:38 +0200

    label is being displayed successfully

--- game.py
+++ game.py
@@ -3,7 +3,16 @@
 
 import cocos
 
-class PyFense(cocos.layer.Layer):
+class PyFenseUi(cocos.layer.Layer):
 	def __init__(self):
-		super(PyFense, self).__init__()
-		
+		super(PyFenseUi, self).__init__()
+		welcomeLabel = cocos.text.Label('Welcome to PyFense', 
+										anchor_x='center', anchor_y='center')
+		welcomeLabel.position = 200, 200
+		self.add(welcomeLabel)
+
+cocos.director.director.init()
+pyfense_ui_layer = PyFenseUi()
+
+main_scene = cocos.scene.Scene(pyfense_ui_layer)
+cocos.director.director.run(main_scene)
