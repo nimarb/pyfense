0e719fea5140093ed9a5bd2e04b1c38e942db34d
Drukob <jakob.mueller@tum.de>
2015-05-21 22:56:19 +0200

    Created Level-Select Screen as Menu to select the level, which then gets printed to the console for testing

--- pyfense_level.py
+++ pyfense_level.py
@@ -3,8 +3,23 @@
 # contains and handles the map selector
 
 import cocos
-from cocos import layer
+from cocos import menu
+from cocos.director import director
 
-class PyFenseLevel(layer.Layer):
+
+class PyFenseLevel(menu.Menu):
     def __init__(self):
-        super(PyFenseLevel, self).__init()__
+        super(PyFenseLevel, self).__init__("PyFenseLevel")
+        lvl1 = menu.ImageMenuItem("assets/lvl1.png", lambda: self.start(1))
+        lvl2 = menu.ImageMenuItem("assets/lvl2.png", lambda: self.start(2))
+        lvl3 = menu.ImageMenuItem("assets/lvl3.png", lambda: self.start(3))
+        lvl4 = menu.ImageMenuItem("assets/lvl4.png", lambda: self.start(4))
+        back = menu.MenuItem("Back to menu", self.on_quit)
+        menuItems = [lvl1, lvl2, lvl3, lvl4, back]
+        self.create_menu(menuItems)
+
+    def start(self, lvl):
+        print("Start Game with map #%d" % lvl)
+
+    def on_quit(self):
+        director.pop()
