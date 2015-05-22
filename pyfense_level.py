5181598c991a5804d2c48ba07b3b21207200fc85
Nimar <l.nimar.b@gmail.com>
2015-05-22 21:11:37 +0200

    temporarily changed images to display, old ones too largegit add pyfense_level.py git add pyfense_level.py

--- pyfense_level.py
+++ pyfense_level.py
@@ -10,10 +10,10 @@ from cocos.director import director
 class PyFenseLevel(menu.Menu):
     def __init__(self):
         super(PyFenseLevel, self).__init__("Select A Level")
-        lvl1 = menu.ImageMenuItem("assets/lvl1.png", lambda: self.start(1))
-        lvl2 = menu.ImageMenuItem("assets/lvl2.png", lambda: self.start(2))
-        lvl3 = menu.ImageMenuItem("assets/lvl3.png", lambda: self.start(3))
-        lvl4 = menu.ImageMenuItem("assets/lvl4.png", lambda: self.start(4))
+        lvl1 = menu.ImageMenuItem("assets/tower.png", lambda: self.start(1))
+        lvl2 = menu.ImageMenuItem("assets/tower.png", lambda: self.start(2))
+        lvl3 = menu.ImageMenuItem("assets/enemy.png", lambda: self.start(3))
+        lvl4 = menu.ImageMenuItem("assets/enemy.png", lambda: self.start(4))
         back = menu.MenuItem("Back to menu", self.on_quit)
 
         width, height = director.get_window_size()
