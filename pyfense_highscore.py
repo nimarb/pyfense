62886ce0eee4b28a84967c85b67e047d7df1aab7
Robin Wachsmann <robin.wachsmann@tum.de>
2015-05-24 16:40:27 +0200

    highscore alpha not working

--- pyfense_highscore.py
+++ pyfense_highscore.py
@@ -3,7 +3,23 @@
 
 import cocos
 from cocos import layer
+from cocos import menu
 
 class PyFenseHighscore(layer.Layer):
     def __init__(self):
-        super().__init()__
+        super().__init__("Highscore")
+        entry = []
+        highscore = readFile("data/highscore.txt")
+        hs_splitted = [row.split(", ") for row in highscore]
+        for i in range(1,5):
+            entry[i] = menu.MenuItem(highscore[i])
+        
+        menuItems = [entry[1], entry[2], entry[3], entry[4], entry[5]]
+        self.create_menu(menuItems)
+
+
+def readFile(fileName):
+
+    with open(fileName, "r") as openedFile:
+        fileData = openedFile.readlines()
+    return fileData
