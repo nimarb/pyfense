c19ac2feee62cfdac48447f92f6a1d6e879374ff
dan-tum <lechner.daniel@hotmail.de>
2015-05-24 22:10:47 +0200

    Create drawingTrial

---
+++ drawing_trial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import cocos
+from cocos.director import director
+from cocos.actions import *
+from cocos import layer
+from cocos import sprite
+
+    
+settings = {
+	"window": {
+		"width": 1024,
+		"height": 768,
+		"caption": "PyFense",
+		"vsync": True,
+		"fullscreen": False,
+		#ATTENTION: misspelling intentional, pyglet fcked up
+		"resizable": True
+	}, 
+	"world": {
+		"gameSpeed": 1.0
+	},
+	"player": {
+		"currency": 200	
+	},
+	"general": {
+		"showFps" : True
+	}
+    }
+    
+    
+def main():    
+    director.init(**settings["window"])
+    layer_menu = drawingTrial()
+    scene_menu = cocos.scene.Scene(layer_menu)
+    director.run(scene_menu)
+    
+    
+class drawingTrial(layer.Layer):
+     def __init__(self):
+         super().__init__()
+         background = sprite.Sprite('assets/tower.png')
+         background.position = 320,240
+         background.scale = 1
+         self.add(background, z=1)
+         scale = scaleBy(3, duration=4)
+         background.do( Repeat(scale+Reverse(scale)))
+         #background.draw()
+         
+    
+   
+if __name__ == "__main__":
+    main()
