0834fff550f2b671fbe1d3f7112300fe4834c9ce
Nimar <l.nimar.b@gmail.com>
2015-05-24 20:54:18 +0200

    initial commit of pyfense_game

--- pyfense_game.py
+++ pyfense_game.py
@@ -1,2 +1,20 @@
 # pyfense_game.py
-# contains PyFenseGame class (scene/layer)
+# contains PyFenseGame class (scene)
+
+import cocos
+import pyglet
+from cocos.director import director
+from cocos.scene import Scene
+from pyfense_map import *
+
+class PyFenseGame(scene.Scene):
+	def __init__(self, levelNumber):
+		super.__init__()
+		self.levelMap = "level_" + levelNumber
+		
+	def loadMap():
+		self.levelMap = PyFenseMap(self.levelMap)
+		
+	def get_level_number():
+		return self.levelNumber
+	
