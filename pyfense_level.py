37a19dbc6d446d44645431c618c21a9a6cedb14b
Nimar <l.nimar.b@gmail.com>
2015-05-24 22:19:01 +0200

    fixed print statement order

--- pyfense_level.py
+++ pyfense_level.py
@@ -6,6 +6,7 @@ import cocos
 from cocos import menu
 from cocos.director import director
 from cocos import scene
+from cocos.scenes import SplitRowsTransition
 
 from pyfense_game import *
 
@@ -36,9 +37,9 @@ class PyFenseLevel(menu.Menu):
         self.create_menu(menuItems)
 
     def start(self, lvl):
+        print("Start Game with map #%d" % lvl)
         director.replace(SplitRowsTransition(PyFenseGame(lvl),
                             duration=1))
-        print("Start Game with map #%d" % lvl)
 
     def on_quit(self):
         director.pop()
