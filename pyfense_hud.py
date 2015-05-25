bad6eb8e91f1e5a6245e03f980874669f38c73ee
Nimar <l.nimar.b@gmail.com>
2015-05-26 00:31:56 +0200

    initial HUD commit

---
+++ pyfense_hud.py
@@ -0,0 +1,9 @@
+# pyfense_hud.py
+# contains head up display, takes care of displaying lose/win
+# current wave number and other information intented for the player
+
+import cocos
+
+class PyFenseHud(cocos.layer.Layer):
+    def __init__():
+        super().__init__()
