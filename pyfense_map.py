850ae4cf98ceda1e99ee83fba65c33a56874a771
Nimar <l.nimar.b@gmail.com>
2015-05-25 21:51:17 +0200

    basic map bg image display working

--- pyfense_map.py
+++ pyfense_map.py
@@ -5,7 +5,7 @@ import pyglet
 from pyglet.image.codecs.png import PNGImageDecoder
 import cocos
 
-class PyFenseMap(cocos.layer.ScrollableLayer):
+class PyFenseMap(cocos.layer.Layer):
 	def __init__(self, levelMap):
 		super().__init__()
 		self.levelMap = levelMap
@@ -16,9 +16,24 @@ class PyFenseMap(cocos.layer.ScrollableLayer):
 		#TODO: error handling for no image available case!
 		#use python's PNGImageDecoder due to segfault causing bug in gdk_pixbuf2
 		self.backgroundImage = pyglet.image.load("assets/" + self.levelMap + "_test.png", decoder=PNGImageDecoder())
-		#self.backgroundImage = pyglet.image.load("assets/background.png", decoder=PNGImageDecoder())
+		self.backgroundSprite = cocos.sprite.Sprite(self.backgroundImage)
 		
 	def drawBackgroundImage(self):
-		self.backgroundSprite = cocos.sprite.Sprite(self.backgroundImage)
-		self.backgroundSprite.postition = 300, 330
+		w, h = cocos.director.director.get_window_size()
+		self.backgroundSprite.position = w/2, h/2
 		self.add(self.backgroundSprite, z=0)
+		self.scaleBackgroundToWindow()
+		
+	def scaleBackgroundToWindow(self):
+		#TODO: keep screen filled with bg image, no matter resize
+		#TODO: to eventually allow scrolling, apply "too large" scaling factor
+		img_w = self.backgroundImage.width
+		img_h = self.backgroundImage.height
+		imgRatio = img_w / img_h
+		screen_w, screen_h = cocos.director.director.get_window_size()
+		screenRatio = screen_w / screen_h
+		if imgRatio < screenRatio:
+			self.scaleRatio = screen_h / img_h
+		else:
+			self.scaleRatio = screen_w / img_w
+		self.backgroundSprite.scale = self.scaleRatio
