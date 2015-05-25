f34faf9057a2db1b9e30f3583431a1a16c68bfec
Nimar <l.nimar.b@gmail.com>
2015-05-25 19:55:58 +0200

    now able to load a map

--- pyfense_game.py
+++ pyfense_game.py
@@ -4,17 +4,17 @@
 import cocos
 import pyglet
 from cocos.director import director
-from cocos.scene import Scene
+from cocos import scene
 from pyfense_map import *
 
 class PyFenseGame(scene.Scene):
 	def __init__(self, levelNumber):
-		super.__init__()
-		self.levelMap = "level_" + levelNumber
+		super().__init__()
+		print(levelNumber)
+		self.levelMapName = "lvl" + str(levelNumber)
+		self.loadMap()
 		
-	def loadMap():
-		self.levelMap = PyFenseMap(self.levelMap)
+	def loadMap(self):
+		self.levelMap = PyFenseMap(self.levelMapName)
+		self.add(self.levelMap, z=1)
 		
-	def get_level_number():
-		return self.levelNumber
-	
