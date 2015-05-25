ea7189ccef6989195d5bd017c5cf2c24cedc29c1
Nimar <l.nimar.b@gmail.com>
2015-05-25 19:58:15 +0200

    initial about menu entry, still much to be done

--- pyfense_about.py
+++ pyfense_about.py
@@ -1,9 +1,28 @@
 # pyfense_about.py
-# contains PyFenseAbout class (layer)
+# contains PyFenseAbout class (menu)
 
-from cocos import layer
+import cocos
+from cocos import menu
+from cocos import text
 
-class PyFenseAbout(layer.Layer):
+import pyglet
+
+class PyFenseAbout(menu.Menu):
     def __init__(self):
-        super(PyFenseAbout, self).__init__()
-
+        super().__init__("About PyFense")
+        back = menu.MenuItem("Back", self.back)
+        abtText = menu.MenuItem("PyFense is a Tower Defense style game made by five students at TUM", self.void)
+        #abtText.label.position = 512, 600
+        #self.super(abtText, self).item["font_size"] = 14
+        #aboutText = text.Label("PyFense is a Tower Defense style game made by five students at TUM", 
+        #    font_size=14, anchor_x='center', anchor_y='center')
+        #aboutText = pyglet.text.Label("asdasdsd", font_size=14, anchor_x='center', anchor_y='center')
+        #aboutText.position = 512, 600
+        self.create_menu([back, abtText])
+        #self.add(aboutText)
+        
+    def back(self):
+        cocos.director.director.pop()
+    def void(self):
+        return True
+    
