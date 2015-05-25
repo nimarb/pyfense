c1ed2e299f36ec3f08542dc4ad09c704885917f5
dan-tum <lechner.daniel@hotmail.de>
2015-05-26 00:22:35 +0200

    PyFense subclass working

--- drawing_trial.py
+++ drawing_trial.py
@@ -6,6 +6,7 @@ from cocos.actions import *
 from cocos import layer
 from cocos import sprite
 from cocos.actions import *
+import math
 
     
 settings = {
@@ -38,29 +39,144 @@ def main():
     
     
 class drawingTrial(layer.Layer):
-     def __init__(self):
-         super().__init__()
-         background = sprite.Sprite('assets/background.png')
-         background.position = settings["window"]["width"]/2, settings["window"]["height"]/2
-         background.scale = settings["window"]["height"]/background.height
-         self.add(background, z=0)
+    def __init__(self):
+        super().__init__()
+       
+        background = PySprite('assets/background.png', 'center', 'fitHeight')
+        enemy = PySprite('assets/enemy_ghost.png', (300,200) , 2)
+        projectile = PySprite('assets/projectile.png', 'center', 0.3)
+        self.add(background, z = 0)
+        self.add(enemy, z=1)
+        self.add(projectile, z= 2)
+
+
+
+
+      #Move sprites
          
-         projectile = sprite.Sprite('assets/projectile.png')
-         projectile.position = settings["window"]["width"]/2, settings["window"]["height"]/2
-         projectile.scale = 0.5
-         self.add(projectile, z=1)
-         
-         move = MoveTo((30, 40), 3)
-         projectile.do(move)
+        #Move to certain position in certain duration
+        #move = MoveTo(enemy.position, 2)
+        #projectile.do(move)
          
          
+        #Move Projectile to position with certain velocity
+#        speed = 100
+ #       dist = distance(enemy.position, projectile.position)
+  #      duration = dist/speed
+   #     projectile.do(MoveTo(enemy.position, duration))
+
+        #scale = ScaleBy(3, duration=4)
+        #background.do(  scale + Reverse(scale) )
+         
+        #background.draw()
+        
+#    def distance((x1, y1), (x2, y2)):
+#       return sqrt( (x2 - x1)**2 + (y2-y1)**2)
+
+         
+    
+class PySprite(sprite.Sprite):
+    
+    #Adds a Sprite with filepath
+    def __init__(self, filepath, position, scale):
+        super().__init__(filepath)
+                
+        #Recognize 'center' as keyword for position
+        if position == 'center':
+            self.position =  director.get_window_size()[0]/2, director.get_window_size()[1]/2 
+        else:
+            self.position = position
+            
+        #Recognize 'fitHeight' and'fitWidth' as keywords for scale
+        if scale == 'fitHeight':
+            self.scale = director.get_window_size()[1]/self.height
+        elif scale == 'fitWidth':
+            self.scale = director.get_window_size()[0]/self.width
+        else:
+            self.scale = scale
+                
+        
+        
+        
+        
+        
+ 
+
+
+        
+        
+    
+    
+    
+
+
+
+
+
+
+        
       
-         #scale = ScaleBy(3, duration=4)
-         #background.do(  scale + Reverse(scale) )
+#    def drawSprite(self, filepath, position, scale, z):
+#        self.filepath = filepath
+#        self.z = z
+#        
+#        
+#        #Recognize 'fitHeight' and 'fitWidth' as keyword for scale
+##        if scale == 'fitHeight':
+##             self.scale = settings["window"]["height"]/self.image.height
+##        elif scale == 'fitWidth':
+##             self.scale = settings["window"]["width"]/self.image.width
+##        else:
+#        self.scale = scale
+#        
+#        #Recognize 'center' as keyword for position
+#        if position == 'center':
+#            self.position = settings["window"]["width"]/2, settings["window"]["height"]/2
+#           
+#        else:
+#            self.position = position
+#            
+#        print(self.filepath)
+#        print(self.position)
+#        print(self.scale)
+#        print(self.z)
+#        
+#        image = sprite.Sprite(filepath)
+#
+#        image.position = 0,0
+#        image.scale = 6
+#        
+#        print(image.position)
+#        
+#        self.add(image, self.z)
+#        
+#        
+        
+        #Import and draw sprites
+#        background = sprite.Sprite('assets/background.png')
+#        background.scale = settings["window"]["height"]/background.height        
+#        background.position = settings["window"]["width"]/2, settings["window"]["height"]/2
+#  
+#        self.add(background, z=0)
+#         
+#        projectile = sprite.Sprite('assets/projectile.png')
+ #       projectile.position = settings["window"]["width"]/2, settings["window"]["height"]/2
+  #      projectile.scale = 0.2
+   #     self.add(projectile, z=2)
          
-         #background.draw()
+#        enemy = sprite.Sprite('assets/enemy_ghost.png')
+ #       enemy.position = 300, 200
+  #      enemy.scale = 2
+   #     self.add(enemy, z=1)
+         
+         
+
+             
+              
+      
+         
          
     
    
 if __name__ == "__main__":
-    main()
+    main()        
