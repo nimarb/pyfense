d40b8407b886c432cc86cb8ca5591ece28b215a7
Nimar <l.nimar.b@gmail.com>
2015-05-26 01:26:12 +0200

    basic hud working

--- pyfense_hud.py
+++ pyfense_hud.py
@@ -5,5 +5,14 @@
 import cocos
 
 class PyFenseHud(cocos.layer.Layer):
-    def __init__():
+    def __init__(self):
         super().__init__()
+        self.currentWave = 1
+        self.displayWaveNumber()
+        
+    def displayWaveNumber(self):
+        self.waveLabel = cocos.text.Label('Current Wave: ' + str(self.currentWave), 
+                anchor_x='center', anchor_y='center')
+        w, h = cocos.director.director.get_window_size()
+        self.waveLabel.position = w / 2, h - 30
+        self.add(self.waveLabel)
