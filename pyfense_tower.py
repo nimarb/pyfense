3cb10c3fe43a19b6c33da9e4971c00495c64faf8
Drukob <jakob.mueller@tum.de>
2015-05-26 16:34:46 +0200

    fire-function implemented, gets called ever firerate seconds

--- pyfense_tower.py
+++ pyfense_tower.py
@@ -1,18 +1,24 @@
 import cocos
 from cocos import sprite
 import pyglet
-#import os
+# import os
 from pyglet.image.codecs.png import PNGImageDecoder
+import pyfense_entities
+import pyfense_projectile
+import pyfense_game
+from cocos.director import clock
 
-#__path__ = os.path.dirname(os.path.abspath(__file__))
-#DATA = os.path.abspath(os.path.join(__path__,'assets','tower.png'))
-#texture = pyglet.image.load(DATA)
+# __path__ = os.path.dirname(os.path.abspath(__file__))
+# DATA = os.path.abspath(os.path.join(__path__,'assets','tower.png'))
+# texture = pyglet.image.load(DATA)
 texture = pyglet.image.load("assets/tower.png", decoder=PNGImageDecoder())
 
-#The towers with dummy values 
-#Is a cocos.sprite.Sprite
-#Needs position in tuple (posx,posy)
-#Takes tower.png found in assets directory
+# The towers with dummy values
+# Is a cocos.sprite.Sprite
+# Needs position in tuple (posx,posy)
+# Takes tower.png found in assets directory
+
+
 class PyFenseTower(sprite.Sprite):
     def __init__(self, position):
         super(PyFenseTower, self).__init__(texture, position)
@@ -23,25 +29,38 @@ class PyFenseTower(sprite.Sprite):
         self.posx = position[0]
         self.posy = position[1]
         self.cost = 100
-    
-    #get the current values of this tower    
+        clock.schedule_interval(self.fire, self.firerate)
+
+    def fire(self, dt):
+        enemies = pyfense_entities.PyFenseEntities.enemies
+        print(enemies)
+        if(not enemies):
+            pass
+        else:
+            target = self.find_next_enemy(enemies).position
+            projectile = pyfense_projectile.PyFenseProjectile(target, (self.posx, self.posy))
+            print(projectile)
+            # funktioniert nicht, weil entityMap nicht gefunden wird?
+            # pyfense_game.entityMap.add(projectile)
+
+    # get the current values of this tower
     def get_values(self):
-        values = [self.level, self.damage, self.rangeradius, self.firerate, 
-                  self.cost] 
+        values = [self.level, self.damage, self.rangeradius, self.firerate,
+                  self.cost]
         return values
 
-    #get the values of this tower thatwould be after an upgrade
+    # get the values of this tower thatwould be after an upgrade
     def get_previewvalues(self):
         preview_level = self.level+1
         preview_damage = self.damage + self.level*2
         preview_firerate = self.firerate + 1
         preview_rangeradius = self.rangeradius + 2
         preview_cost = self.cost*2
-        preview_values = [preview_level, preview_damage, preview_firerate, 
+        preview_values = [preview_level, preview_damage, preview_firerate,
                           preview_rangeradius, preview_cost]
         return preview_values
-       
-    #upgrade this tower and increase the values
+
+    # upgrade this tower and increase the values
     def upgrade_Tower(self):
         values = self.get_previewvalues()
         self.level = values[0]
@@ -49,32 +68,32 @@ class PyFenseTower(sprite.Sprite):
         self.firerate = values[2]
         self.rangeradius = values[3]
         self.cost = values[4]
-    
-    #find the next enemy (that should be attacked next) 
-    #needs an array with all enemies
-    #Dummy values at the moment
+
+    # find the next enemy (that should be attacked next)
+    # needs an array with all enemies
+    # Dummy values at the moment
     def find_next_enemy(self,enemies):
         nearestenemy = enemies[0]
         for enemy in enemies:
             if enemy.y < self.posy and enemy.x < self.posx:
                 if enemy.y < nearestenemy.y and enemy.x < nearestenemy.x:
                     nearestenemy = enemy
-                
+        return nearestenemy
+
     def get_posx(self):
         return self.posx
-        
+
     def get_posy(self):
-        return self.posy    
-    
+        return self.posy
+
     def get_damage(self):
         return self.damage
-        
+
     def get_range(self):
         return self.rangeradius
-        
+
     def get_firerate(self):
         return self.firerate
-        
+
     def get_level(self):
         return self.level
-   
