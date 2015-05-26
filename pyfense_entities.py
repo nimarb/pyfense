280bb016e1f18cb0b46f6e7910ec9c8e341413a8
Drukob <jakob.mueller@tum.de>
2015-05-26 16:37:41 +0200

    towers added to towerlist, enemie saved in a class-attribute

--- pyfense_entities.py
+++ pyfense_entities.py
@@ -8,21 +8,33 @@ from pyfense_tower import *
 from pyfense_enemy import *
 from pyfense_projectile import *
 
+
+
 class PyFenseEntities(cocos.layer.Layer):
+
+    enemies = []
     def __init__(self):
         super().__init__()
-        self.tower = self.placeTower(40, 30)
 
         # create new enemy every x seconds
-        self.enemies = []
+        self.__class__.enemies = []
+        self.towers = []
         clock.schedule_interval(self.addEnemy, 5)
 
 
+        self.tower = self.placeTower(40, 30)
+
     def placeTower(self, pos_x, pos_y):
-        self.add(PyFenseTower((pos_x, pos_y)))
+        t1 = PyFenseTower((pos_x, pos_y))
+        self.towers.append(t1)
+        self.add(t1)
+
+    def fire(self, dt):
+        for tower in self.towers:
+            tower.fire(self.enemies)
+
 
     def addEnemy(self, dt):
         enemy = PyFenseEnemy(1, 1)
         self.enemies.append(enemy)
         self.add(enemy)
-
