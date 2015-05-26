2f8743173b8656c9d5d2debbae29adc498865418
Drukob <jakob.mueller@tum.de>
2015-05-26 12:27:43 +0200

    Enemy move on  hard-coded path

--- pyfense_enemy.py
+++ pyfense_enemy.py
@@ -1,2 +1,29 @@
 # pyfense_enemy.py
 # contains PyFenseEnemy class
+import cocos
+import pyglet
+from cocos import sprite
+from cocos import actions
+from pyglet.image.codecs.png import PNGImageDecoder
+
+
+image = pyglet.image.load("assets/enemy.png", decoder=PNGImageDecoder())
+
+
+class PyFenseEnemy(sprite.Sprite):
+    def __init__(self, lvl, wave):
+        self.curpos = (0, 340)
+        super(PyFenseEnemy, self).__init__(image, position = self.curpos)
+        self.hitpoints = 10
+        self.speed = 10
+        self.reward = 20
+        self.damage = 1
+        self.move(lvl)
+
+    movePathlvl1 = (actions.MoveBy((195, 0)) + actions.MoveBy((0, 230))
+                    + actions.MoveBy((230, 0)) + actions.MoveBy((0, -300))
+                    + actions.MoveBy((300, 0)) + actions.MoveBy((0, 130))
+                    + actions.MoveBy((400, 0), 3))
+
+    def move(self, lvl):
+        self.do(self.movePathlvl1)
