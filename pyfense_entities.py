8eb3dd52bab847b7d4cd8006a9bfa69fae00939c
Nimar <l.nimar.b@gmail.com>
2015-05-27 00:20:41 +0200

    formatting changed, image load method altered

--- pyfense_entities.py
+++ pyfense_entities.py
@@ -1,35 +1,33 @@
 # pyfense_entities.py
 # contains the layer on which all enemies and towers are placed (layer)
 
+from pyglet.image.codecs.png import PNGImageDecoder
+import math
+
 import cocos
 from cocos.director import clock
 
 from pyfense_tower import *
 from pyfense_enemy import *
 from pyfense_projectile import *
-import math
-
 
 
 class PyFenseEntities(cocos.layer.Layer):
-
     enemies = []
+    
     def __init__(self):
         super().__init__()
-
         # create new enemy every x seconds
         self.__class__.enemies = []
         self.towers = []
         clock.schedule_interval(self.addEnemy, 0.5)
-
-
         self.tower = self.placeTower(40, 30)
         clock.schedule_interval(self.drawProjectiles, 1)
         clock.schedule_interval(self.checkCollision, 0.1)
 
-
     def distance(self, a, b):
         return(math.sqrt((a.x-b.x)**2+(a.y-b.y)**2))
+        
 
     def checkCollision(self, dt):
         for t in self.towers:
@@ -44,33 +42,32 @@ class PyFenseEntities(cocos.layer.Layer):
                         enemy.kill()
                         t.projectilelist.remove(p)
                         p.kill()
-                        self.startAnimation(enemy.position)
-                        
-
+                        self.startAnimation(enemy.position)    
+                                         
 
     def placeTower(self, pos_x, pos_y):
         t1 = PyFenseTower((pos_x, pos_y))
         self.towers.append(t1)
         self.add(t1)
+        
 
     def drawProjectiles(self, dt):
         for t in self.towers:
             for p in t.projectilelist:
                 self.add(p, z = 1)
-
+                
 
     def addEnemy(self, dt):
         enemy = PyFenseEnemy(1, 1)
         self.enemies.append(enemy)
         self.add(enemy)
-
-
+        
 
     def startAnimation(self, position):
         #ANIMATION FOR EXPLOSION
         
         # load the example explosion as a pyglet image
-        spritesheet = pyglet.image.load('assets/explosion01_128.png')
+        spritesheet = pyglet.image.load('assets/explosion01_128.png', decoder=PNGImageDecoder())
             
         # use ImageGrid to divide your sprite sheet into smaller regions
         grid = pyglet.image.ImageGrid(spritesheet, 10, 10, item_width=128, item_height=128)
