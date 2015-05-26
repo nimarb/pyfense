a6e18b7faf1bb90c3eb5772b09377c5d83185262
Drukob <jakob.mueller@tum.de>
2015-05-26 12:34:06 +0200

    New enemies spawn every 5 seconds

--- pyfense_entities.py
+++ pyfense_entities.py
@@ -2,6 +2,7 @@
 # contains the layer on which all enemies and towers are placed (layer)
 
 import cocos
+from cocos.director import clock
 
 from pyfense_tower import *
 from pyfense_enemy import *
@@ -12,6 +13,16 @@ class PyFenseEntities(cocos.layer.Layer):
         super().__init__()
         self.tower = self.placeTower(40, 30)
 
+        # create new enemy every x seconds
+        self.enemies = []
+        clock.schedule_interval(self.addEnemy, 5)
+
+
     def placeTower(self, pos_x, pos_y):
         self.add(PyFenseTower((pos_x, pos_y)))
-        
+
+    def addEnemy(self, dt):
+        enemy = PyFenseEnemy(1, 1)
+        self.enemies.append(enemy)
+        self.add(enemy)
+
