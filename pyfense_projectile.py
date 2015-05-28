95af9fc95b4f5abda08ab711f3da020ff138de69
Nimar <l.nimar.b@gmail.com>
2015-05-28 16:47:27 +0200

    simplified projectile

--- pyfense_projectile.py
+++ pyfense_projectile.py
@@ -12,20 +12,15 @@ import pyglet
 class PyFenseProjectile(sprite.Sprite):
     def __init__(self, target, origin, velocity):
         super().__init__("assets/projectile0.png", position = origin, scale = 0.3)
-        
-        self.velocity = velocity
-        self.target = target
-        self.moveVel(self, self.target, self.velocity)
+        self.moveVel(self, target, velocity)
         
         #self.kill
         
     # Move to position of target with certain velocity    
-    def moveVel(self, projectile, enemy, velocity):
-        dist = self.distance(self.target, self.position)
+    def moveVel(self, projectile, target, velocity):
+        dist = self.distance(target, self.position)
         duration = dist/velocity
-        projectile.do(MoveTo(self.target, duration))
-        
-        
+        projectile.do(MoveTo(target, duration))
         
     def distance(self, a, b):
         return math.sqrt( (b[0] - a[0])**2 + (b[1]-a[1])**2)
