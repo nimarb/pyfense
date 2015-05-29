8175bc60d0ce1dc8d84dc49c22054fb28ee04e38
Nimar <l.nimar.b@gmail.com>
2015-05-29 13:55:55 +0200

    began grid functionality

--- pyfense_game.py
+++ pyfense_game.py
@@ -3,8 +3,10 @@
 
 import cocos
 import pyglet
+import math
 from cocos.director import director
 from cocos import scene
+
 from pyfense_map import *
 from pyfense_entities import *
 from pyfense_hud import *
@@ -16,6 +18,15 @@ class PyFenseGame(scene.Scene):
 		self.loadMap()
 		self.displayEntities()
 		self.displayHud()
+		#initialise game grid to store where enemies can walk, 
+		# towers can be build and where towers are already built
+		#one grid cell is 60x60px large (full hd resolution scale) 
+		#key: 
+		#0 := no tower can be build, no enemy can walk
+		#1 := no tower can be build, enemy can walk
+		#2 := tower can be build, no enemy can walk
+		#3 := tower has been built, no enemy can walk, no tower can be build (can upgrade (?))
+		self.gameGrid = [[0 for x in range (8)] for x in range(16)]
 		
 	def loadMap(self):
 		self.levelMap = PyFenseMap(self.levelMapName)
@@ -29,6 +40,20 @@ class PyFenseGame(scene.Scene):
 		self.hud = PyFenseHud()
 		self.hud.push_handlers(self)
 		self.add(self.hud, z=2)
+		
+	def setGridPix(self, x, y, kind)
+		if kind < 0 or kind > 3:
+			print("WRONG GRID TYPE, fix ur shit")
+			return
+		grid_x = math.abs(x / 60)
+		grid_y = math.abs(y / 60)
+		self.setGrid(grid_x, grid_y, kind)
+		
+	def setGrid(self, grid_x, grid_y, kind):
+		if kind < 0 or kind > 3:
+			print("WRONG GRID TYPE, fix ur shit")
+			return
+		self.gameGrid[grid_x][grid_y] = kind
 		
 	def on_build_tower(self, towerNumber, pos_x, pos_y):
 		self.entityMap.buildTower(towerNumber, pos_x, pos_y)
