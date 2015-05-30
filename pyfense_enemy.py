8e026440d6a66ef1a79fdf8afaaf75b9020fe45a
Nimar <l.nimar.b@gmail.com>
2015-05-31 01:32:42 +0200

    prototype for floating health bars

--- pyfense_enemy.py
+++ pyfense_enemy.py
@@ -10,11 +10,11 @@ from pyglet.image.codecs.png import PNGImageDecoder
 class PyFenseEnemy(sprite.Sprite):
     def __init__(self, lvl, wave):
         # TODO: Different assets and values for stronger enemies to be loaded from textfile
-        self.curpos = (0, 340) 
+        self.currentPos = (0, 340) 
         image = pyglet.image.load("assets/enemy1.png", decoder=PNGImageDecoder())
-        super(PyFenseEnemy, self).__init__(image, position = self.curpos)
-        
-        self.healthpoints = 40
+        super(PyFenseEnemy, self).__init__(image, position = self.currentPos)
+        self.healthPoints = 40
+        #self.drawHealthBar()
         self.speed = 10
         self.reward = 20
         self.damage = 1
@@ -27,3 +27,11 @@ class PyFenseEnemy(sprite.Sprite):
 
     def move(self, lvl):
         self.do(self.movePathlvl1)
+        
+    def drawHealthBar(self):
+        (x, y) = self.position
+        bar_x = x - self.width / 2 
+        bar_y = y + self.height / 2 + 20
+        #print("barx: " + str(bar_x) + ", bary: " + str(bar_y))
+        self.healthBar = cocos.draw.Line((bar_x, bar_y), (bar_x + self.healthPoints, bar_y), (123, 123, 123, 123), 15)
+        return self.healthBar
