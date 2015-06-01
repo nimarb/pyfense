3c02cc0464073d6454d23a475db6d431a273a0f3
dan-tum <lechner.daniel@hotmail.de>
2015-06-01 18:44:44 +0200

    Change way of loading files

--- pyfense_resources.py
+++ pyfense_resources.py
@@ -1,41 +1,37 @@
 # Assets are loaded in this file and used throughout the application efficiently
 
-
 import pyglet
 from pyglet.image.codecs.png import PNGImageDecoder	
 
+def loadImage(filename):
+    return pyglet.image.load(filename, decoder=PNGImageDecoder())
+
+def loadAnimation(filepath, spritesheet_x, spritesheet_y, width, height, duration, loop):
+    spritesheet = pyglet.image.load(filepath, decoder=PNGImageDecoder())
+    grid = pyglet.image.ImageGrid(spritesheet, 
+                              spritesheet_y, spritesheet_x, item_width=width, item_height=width)
+    textures = pyglet.image.TextureGrid(grid)
+    images = textures[0:len(textures)]
+    return pyglet.image.Animation.from_image_sequence(
+            images, duration, loop=loop)                      
+    
+    
 background = {
-    "lvl1" : pyglet.image.load("assets/lvl1.png", decoder=PNGImageDecoder()),
-    "lvl2" : pyglet.image.load("assets/lvl2.png", decoder=PNGImageDecoder()),
-    "lvl3" : pyglet.image.load("assets/lvl3.png", decoder=PNGImageDecoder()),
-    "lvl4" : pyglet.image.load("assets/lvl4.png", decoder=PNGImageDecoder())
+    "lvl1" : loadImage("assets/lvl1.png"),
+    "lvl2" : loadImage("assets/lvl2.png"),
+    "lvl3" : loadImage("assets/lvl3.png"),
+    "lvl4" : loadImage("assets/lvl4.png")
                }
 enemy = []
-enemy.append(pyglet.image.load("assets/enemy0.png", decoder=PNGImageDecoder()))
-enemy.append(pyglet.image.load("assets/enemy1.png", decoder=PNGImageDecoder()))
+enemy.append(loadImage("assets/enemy0.png"))
+enemy.append(loadImage("assets/enemy1.png"))
 
 tower = []
-tower.append(pyglet.image.load("assets/tower0.png", decoder=PNGImageDecoder()))
-tower.append(pyglet.image.load("assets/tower1.png", decoder=PNGImageDecoder()))
-tower.append(pyglet.image.load("assets/tower2.png", decoder=PNGImageDecoder()))
+tower.append(loadImage("assets/tower0.png"))
+tower.append(loadImage("assets/tower1.png"))
+tower.append(loadImage("assets/tower2.png"))
 
-projectile = pyglet.image.load("assets/projectile0.png", decoder=PNGImageDecoder())
+projectile = loadImage("assets/projectile0.png")
 
-
-
-#Load ExplosionAnimation
-spritesheet = pyglet.image.load('assets/explosions-pack/spritesheets/explosion-1.png',
-    decoder=PNGImageDecoder())
-# use ImageGrid to divide your sprite sheet into smaller regions
-grid = pyglet.image.ImageGrid(spritesheet, 
-                              1, 8, item_width=32, item_height=32)
-# convert to TextureGrid for memory efficiency
-textures = pyglet.image.TextureGrid(grid)
-# access the grid images as you would items in a list
-# this way you get a sequence for your animation
-# reads from bottom left corner to top right corner
-explosionSprites = textures[0:len(textures)]
-#create pyglet animation objects
-
-explosion = pyglet.image.Animation.from_image_sequence(
-            explosionSprites, 0.03, loop=False)
+explosion = loadAnimation('assets/explosions-pack/spritesheets/explosion-1.png', 
+                   8, 1, 32, 32, 0.03, False)    
