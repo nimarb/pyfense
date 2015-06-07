0b3f53beda43cd1ddf6904271c0734c59a9303c5
dan-tum <lechner.daniel@hotmail.de>
2015-06-07 18:58:59 +0200

    Tower rotates to enemy, new asset for tower1, written first test

--- pyfense_tower.py
+++ pyfense_tower.py
@@ -1,5 +1,6 @@
 import cocos
 from cocos import sprite
+from cocos.actions import *
 import pyglet
 from pyglet.image.codecs.png import PNGImageDecoder
 import weakref
@@ -21,22 +22,22 @@ class PyFenseTower(sprite.Sprite,  pyglet.event.EventDispatcher):
         self.attributes = pyfense_resources.tower[towerNumber][1]
         texture = self.attributes["image"]
         super().__init__(texture, position)
-        # Entity is parent class, that has called the tower, weakref.ref() makes it garbage collector safe
-        # self.entityParent = weakref.ref(entityParent)
         self.enemies = enemies
         self.posx = position[0]
         self.posy = position[1]
-        #self.fire(10)
+        self.rotation = 0
+        self.target = self.find_next_enemy()
         self.schedule_interval(self.fire, self.attributes["firerate"])
+        self.schedule(lambda dt: self.find_next_enemy())
+        self.schedule(lambda dt: self.rotateToTarget())
+        
 
     def fire(self, dt):
-        enemies = self.enemies
-        if(not enemies):  # <- if enemies is not empty
+        if(not self.enemies):  # <- if enemies is not empty
             pass
         else:
-            target = self.find_next_enemy(enemies)
-            if (target is not None):
-                self.dispatch_event('on_projectile_fired', self, target, 
+            if (self.target is not None):
+                self.dispatch_event('on_projectile_fired', self, self.target, 
                                     self.attributes["projectilevelocity"],
                                     self.attributes["damage"])
     def distance(self, a, b):
@@ -45,10 +46,10 @@ class PyFenseTower(sprite.Sprite,  pyglet.event.EventDispatcher):
     # find the next enemy (that should be attacked next)
     # either first enemy in range or nearest Enemy
     # standardvalue is first
-    def find_next_enemy(self, enemies, mode="first"):
-        nextEnemy = None
+    def find_next_enemy(self, mode="first"):
+        self.target = None
         self.dist = self.attributes["range"]
-        for enemy in enemies:
+        for enemy in self.enemies:
             if(enemy.x < cocos.director.director.get_window_size()[0]
                and  # Enemy still in window
                enemy.y < cocos.director.director.get_window_size()[1]):
@@ -58,13 +59,21 @@ class PyFenseTower(sprite.Sprite,  pyglet.event.EventDispatcher):
                         # Check for nearest Enemy
                         # Distance smaller than previous smallest distance
                         if(self.distance(enemy, self) < self.dist):
-                            nextEnemy = enemy
+                            self.target = enemy
                             self.dist = self.distance(enemy, self)
                     elif(mode == "first"):
                         # first Enemy in list, which is in range is the target
-                        nextEnemy = enemy
+                        self.target = enemy
                         break
-        return nextEnemy
+        
+    def rotateToTarget(self):
+        if self.target:
+            x = self.target.x - self.x
+            y = self.target.y - self.y
+            #should actually be atan2(y, x), but then the angle is wrong
+            angle = math.degrees(math.atan2(x, y)) 
+            self.rotation = angle   
+    
 
     # get the current values of this tower
     def get_values(self):
