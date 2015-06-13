c29d99ab05f9fdf2ea3ffe4a3d5a32afac795d64
Robin Wachsmann <robin.wachsmann@tum.de>
2015-06-13 15:54:11 +0200

    pause scene with layer, not working

--- pyfense_pause.py
+++ pyfense_pause.py
@@ -8,13 +8,12 @@ from cocos.text import *
 from cocos.layer import *
 
 class PyFensePause( scene.Scene ):
-<<<<<<< HEAD
     
     def __init( self ):
         
         super().__init()
-        pause = PauseLayer()
-        pause.push_handlers(self)
+        self.pause = PauseLayer()
+        self.pause.push_handlers(self)
         self.add( pause, z=1 )
 
     class PauseLayer( ColorLayer, pyglet.event.EventDispatcher ):
@@ -43,31 +42,3 @@ class PyFensePause( scene.Scene ):
         def on_mouse_release( self, x, y, b, m ):
             director.pop()
             return True
-=======
-
-    is_event_handler = True
-
-    def __init__( self ):
-        super().__init__( )
-        
-    def on_enter ( self ):
-        super( PyFensePause, self ).on_enter()
-        w, h = director.get_window_size()
-        text = Label('+++ Game Paused +++',
-        font_name = 'Arial',
-        font_size = 20,
-        anchor_x = 'center',
-        anchor_y = 'center')
-        text.position = w/2. , h/2.
-        self.add(text)
-
-    def on_key_press( self, k, m ):
-        if k in (key.ENTER, key.ESCAPE, key.SPACE, key.Q ):
-            director.pop()
-            return True
-
-    def on_mouse_release( self, x, y, b, m ):
-        director.pop()
-        return True
-
->>>>>>> master
