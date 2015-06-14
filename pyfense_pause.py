9a6395203ca0f2f7152885d13d1de40f3d241ce9
Robin Wachsmann <robin.wachsmann@tum.de>
2015-06-14 17:36:50 +0200

    error fixes, still no event detection

--- pyfense_pause.py
+++ pyfense_pause.py
@@ -9,36 +9,37 @@ from cocos.layer import *
 
 class PyFensePause( scene.Scene ):
     
-    def __init( self ):
+    def __init__( self ):
         
-        super().__init()
-        self.pause = PauseLayer()
-        self.pause.push_handlers(self)
-        self.add( pause, z=1 )
+        super().__init__()
+        print("Pause Scene loaded")
+        self.add( PauseLayer(), z=1 )
 
-    class PauseLayer( ColorLayer, pyglet.event.EventDispatcher ):
+    def on_enter( self ):
+        pass
+
+class PauseLayer( Layer, pyglet.event.EventDispatcher ):
         
-        is_event_handler = True
+    is_event_handler = True
 
-        def __init__( self ):
-            super().__init__(0,0,0,0.8)
-            
-        def on_enter ( self ):
-            super( PyFensePause, self ).on_enter()
-            w, h = director.get_window_size()
-            text = Label('+++ Game Paused +++',
-                         font_name = 'Arial',
-                         font_size = 20,
-                         anchor_x = 'center',
-                         anchor_y = 'center')
-            text.position = w/2. , h/2.
-            self.add(text)
-            
-        def on_key_press( self, k, m ):
-            if k in (key.ENTER, key.ESCAPE, key.SPACE, key.Q ):
-                director.pop()
-                return True
-                  
-        def on_mouse_release( self, x, y, b, m ):
+    def __init__( self ):
+        super().__init__()
+        w, h = director.get_window_size()
+        text = Label('+++ Game Paused +++',
+                     font_name = 'Arial',
+                     font_size = 20,
+                     anchor_x = 'center',
+                     anchor_y = 'center')
+        text.position = w/2. , h/2.
+        self.add(text)
+        
+    def on_key_press( self, k, m ):
+        if k in (key.ENTER, key.ESCAPE, key.SPACE, key.Q ):
+            print("exit key pressed")
             director.pop()
             return True
+              
+    def on_mouse_release( self, x, y, b, m ):
+        print("mouse to exit used")
+        director.pop()
+        return True
