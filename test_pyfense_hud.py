fc3319f94f0c35176983bb3f6802644ced4334c2
Matthias Lutz <matthias.lutz@tum.de>
2015-06-21 15:29:28 +0200

    pep8+error correction

--- test_pyfense_hud.py
+++ test_pyfense_hud.py
@@ -8,37 +8,38 @@ import pyglet
 from pyfense_hud import *
 
 settings = {
-	"window": {
-		"width": 1920,
-		"height": 1080,
-		"caption": "PyFense",
-		"vsync": True,
-		"fullscreen": False,
-		#ATTENTION: misspelling intentional, pyglet fcked up
-		"resizable": True
-	},
-	"player": {
-		"currency": 200	
-	},
-	"general": {
-		"showFps" : True
-	}
+    "window": {
+        "width": 1920,
+        "height": 1080,
+        "caption": "PyFense",
+        "vsync": True,
+        "fullscreen": False,
+        "resizable": True
+        },
+    "player": {
+        "currency": 200
+        },
+    "general": {
+        "showFps": True
+        }
 }
 
 
 class TestHud(unittest.TestCase):
-	def setUp(self):
-		director.init(**settings['window'])
-		self.hud = PyFenseHud()
-		
-	def test_updateCurrencyNumber(self):
-		self.hud.updateCurrencyNumber(70)
-		result = self.hud.currentCurrency
-		self.assertEqual(70, result)
+    def setUp(self):
+        director.init(**settings['window'])
+        scene = cocos.scene.Scene()
+        director.run(scene)
+        self.hud = PyFenseHud()
 
-	def test_updateNextWaveTimer(self):
-		self.hud.updateNextWaveTimer(1)
-		self.assertEqual(2, self.hud.time)
-	
+    def test_updateCurrencyNumber(self):
+        self.hud.updateCurrencyNumber(70)
+        result = self.hud.currentCurrency
+        self.assertEqual(70, result)
+
+    def test_updateNextWaveTimer(self):
+        self.hud.updateNextWaveTimer(1)
+        self.assertEqual(2, self.hud.time)
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
