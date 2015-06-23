46d18238d7072da07ed0c214ece2246d38df69bc
dan-tum <lechner.daniel@hotmail.de>
2015-06-23 23:46:29 +0200

    PEP8

--- pyfense/pyfense_enemy.py
+++ pyfense/pyfense_enemy.py
@@ -9,7 +9,8 @@ import pyfense_resources
 
 
 class PyFenseEnemy(sprite.Sprite):
-    def __init__(self, position, enemyname, lvl, wave, path, healthMultiplier):
+    def __init__(self, position, enemyname, lvl, wave, path,
+                 healthMultiplier):
         self.attributes = pyfense_resources.enemy[enemyname][lvl]
         self.currentPos = position
         super(PyFenseEnemy, self).__init__(self.attributes["image"],
@@ -30,17 +31,17 @@ class PyFenseEnemy(sprite.Sprite):
     def drawHealthBar(self):
         self.bar_x = self.x - self.healthBarWidth / 2
         self.bar_y = self.y + self.height / 2 + 5
-        healthBarBackground = cocos.draw.Line((self.bar_x, self.bar_y),
-                                         (self.bar_x + self.healthBarWidth,
-                                          self.bar_y),
-                                         (192, 0, 0, 255), 3)
+        healthBarBackground = cocos.draw.Line(
+            (self.bar_x, self.bar_y),
+            (self.bar_x + self.healthBarWidth,
+             self.bar_y), (192, 0, 0, 255), 3)
         healthBarBackground._texture = pyfense_resources.healthBarCap
-        healthBarBackground.visible = False                                 
-        healthBar = cocos.draw.Line((self.bar_x, self.bar_y),
-                                         (self.bar_x + self.healthBarWidth,
-                                          self.bar_y),
-                                         (0, 237, 55, 255), 3)
-        healthBar._texture = pyfense_resources.healthBarCap 
+        healthBarBackground.visible = False
+        healthBar = cocos.draw.Line(
+            (self.bar_x, self.bar_y),
+            (self.bar_x + self.healthBarWidth,
+             self.bar_y), (192, 0, 0, 255), 3)
+        healthBar._texture = pyfense_resources.healthBarCap
         healthBar.visible = False
         # self.healthBar.set_endcap('BUTT_CAP') -> cam be changed by altering
         # the ending sprite which cocos.draw loads
