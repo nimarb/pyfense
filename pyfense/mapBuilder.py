ed7afc96ac37772405d061bd50c11ec17055a384
Jakob Mueller <jakob.mueller@tum.de>
2015-07-01 19:03:23 +0200

    Cleanup and Instruction for the Map Builder

--- pyfense/mapBuilder.py
+++ pyfense/mapBuilder.py
@@ -1,5 +1,14 @@
-# pyfense_game.py
-# contains PyFenseGame class (scene)
+"""
+An experimental Map-Builder to design your own custom level to play the game
+on. Has to be called explicitly by passing the argument 'builder' at startup.
+
+Build your Path by clicking on the respective grid-tiles. Connect the
+start and end Tiles, which are already present on start. Build Places for
+Towers by clicking a second time on the tile. Do not remove or change the
+starting or end points. Do only build unambigous and logical paths (no
+junctions or circles or similar. Press Enter to save the map. On your next
+start the map should be available for playing!
+"""
 import os
 import cocos
 from cocos.director import director
@@ -7,7 +16,7 @@ from cocos.scene import Scene
 from cocos import actions
 from pyfense import resources
 from pyfense import map
-from mapbuilderhud import PyFenseMapBuilderHud
+from pyfense import mapbuilderhud
 import pickle
 
 root = os.path.dirname(os.path.abspath(__file__))
@@ -15,57 +24,39 @@ pathjoin = lambda x: os.path.join(root, x)
 
 
 class PyFenseMapBuilder(Scene):
+    """
+    MapBuilder to create a custom level
+    For Documentation about the CellSelector and Grid Control see game.py
+    """
     is_event_handler = True
 
     def __init__(self):
         super().__init__()
-        # initialise game grid to store where enemies can walk,
-        # towers can be build and where towers are already built
-        # one grid cell is 60x60px large (full hd resolution scale)
-        # gameGrid can be called by using gameGrid[y][x]
-        # key:
-        # 0 := no tower can be build, no enemy can walk
-        # 1 := no tower can be build, enemy can walk
-        # 3 := tower can be build, no enemy can walk
-        # 4 := tower has been built, no enemy can walk,
-        #      no tower can be build (can upgrade (?))
         self.gameGrid = [[0 for x in range(32)] for x in range(18)]
         self.startTile = [8, 0]
         self.endTile = [9, 31]
         self.movePath = actions.MoveBy((0, 0))
-        # self.loadPath()
-        # self.levelMapName = "lvl" + str(levelNumber)
-        # self.loadMap()
         self._display_hud()
         self._load_backgorund()
         self.on_build_path(30, 510)
         self.on_build_path(1890, 570)
 
     def on_save(self):
-        # Save Path in file and "restart" director to update the Menu
+        """
+        Saves the GameGrid-Matrix in a pickle file, so it can be read later to
+        create the correct path from it
+        """
         output = open(pathjoin("data/path.cfg"), "wb")
         pickle.dump(self.gameGrid, output)
         output.close()
         print("save")
         director.pop()
 
-#        scene = Scene()
-#        logo = cocos.sprite.Sprite(resources.logo)
-#        scene.add(logo, z=2)
-#        scene.add(MultiplexLayer(
-#            pyfense.MainMenu(),
-#            pyfense.LevelSelectMenu(),
-#            pyfense.OptionsMenu(),
-#            pyfense.ScoresLayer(),
-#            pyfense.AboutLayer()),
-#            z=1)
-#
-#        director.director.replace(scene)
     def _load_backgorund(self):
         self.add(map.PyFenseMap("background"), z=0)
 
     def _display_hud(self):
-        self.hud = PyFenseMapBuilderHud()
+        self.hud = mapbuilderhud.PyFenseMapBuilderHud()
         self.hud.push_handlers(self)
         self.add(self.hud, z=2)
 
