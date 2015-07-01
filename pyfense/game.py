b155a5512c3894b859717509da79c12d6192d210
Jakob Mueller <jakob.mueller@tum.de>
2015-07-01 18:18:01 +0200

    pythonic formal improvements

--- pyfense/game.py
+++ pyfense/game.py
@@ -2,6 +2,7 @@
 pyfense_game.py
 contains PyFenseGame class (scene)
 """
+import os
 
 from cocos.director import director
 from cocos import scene
@@ -16,9 +17,10 @@ from pyfense import resources
 from pyfense import highscore
 
 # import particles
-import pickle
 import copy
 
+root = os.path.dirname(os.path.abspath(__file__))
+pathjoin = lambda x: os.path.join(root, x)
 
 class PyFenseGame(scene.Scene):
     def __init__(self, levelNumber):
@@ -36,30 +38,25 @@ class PyFenseGame(scene.Scene):
         # no tower can be build (can upgrade (?))
         # 100-200 := 1 + towerNr + towerLvl has been built here
         self.gameGrid = [[0 for x in range(32)] for x in range(18)]
-        if(levelNumber == "custom"):
-            # can only build tower on "grass"
-            pathFile = open("data/path.cfg", "rb")
-            self.gameGrid = pickle.load(pathFile)
-            pathFile.close()
-        else:  # (if levelNumber == 1)
-            self.gameGrid, self.startTile, \
-                self.endTile = resources.initGrid(levelNumber)
+        self.gameGrid, self.startTile, \
+                       self.endTile = resources.initGrid(levelNumber)
 
         resources.loadWaves()
         resources.loadEntities()
-        self.movePath = None
-        self.loadPath()
+
+        self.movePath = self._load_path()
         self.levelMapName = "lvl" + str(levelNumber)
-        self.loadMap()
-        self.displayEntities()
-        self.displayHud()
+        self._load_map()
+        self._display_entities()
+        self._display_hud()
+
         self.currentWave = 0
         self.currentLives = 15
         self.currentCurrency = 300
         highscore.currentWave = self.currentWave
         director.interpreter_locals["game"] = self
 
-    def loadPath(self):
+    def _load_path(self):
         currentTile = copy.deepcopy(self.startTile)
         # move[0] for enemy with rotation and move[1] for healthbar
         move = [[], []]
@@ -68,22 +65,8 @@ class PyFenseGame(scene.Scene):
         while(currentTile[0] != self.endTile[0] or
               currentTile[1] != self.endTile[1]):
 
-            # Left
-            if(self.gameGrid[currentTile[0]][currentTile[1]-1] == 2):
-                # Rotate left
-                move[0].append(actions.RotateTo(180, 0))  # RotateLeft
-                move[1].append([])  # placeholder
-                # Move left
-                for j in range(1, 11):
-                    move[0].append((pos[0]-6*j, pos[1]))
-                    move[1].append((-6, 0))
-                # Next position
-                pos = (pos[0]-60, pos[1])
-                currentTile[1] -= 1
-                self.gameGrid[currentTile[0]][currentTile[1]] = 1
-
             # Right
-            elif(self.gameGrid[currentTile[0]][currentTile[1]+1] == 2):
+            if(self.gameGrid[currentTile[0]][currentTile[1]+1] == 2):
                 # Rotate right
                 move[0].append(actions.RotateTo(0, 0))
                 move[1].append([])
@@ -94,6 +77,19 @@ class PyFenseGame(scene.Scene):
                 # Next position
                 pos = (pos[0]+60, pos[1])
                 currentTile[1] += 1
+                self.gameGrid[currentTile[0]][currentTile[1]] = 1
+            # Left
+            elif(self.gameGrid[currentTile[0]][currentTile[1]-1] == 2):
+                # Rotate left
+                move[0].append(actions.RotateTo(180, 0))  # RotateLeft
+                move[1].append([])  # placeholder
+                # Move left
+                for j in range(1, 11):
+                    move[0].append((pos[0]-6*j, pos[1]))
+                    move[1].append((-6, 0))
+                # Next position
+                pos = (pos[0]-60, pos[1])
+                currentTile[1] -= 1
                 self.gameGrid[currentTile[0]][currentTile[1]] = 1
 
             # Up
@@ -127,13 +123,13 @@ class PyFenseGame(scene.Scene):
             else:
                 break
         self.gameGrid[self.startTile[0]][self.startTile[1]] = 1
-        self.movePath = move
+        return move
 
-    def loadMap(self):
+    def _load_map(self):
         self.levelMap = map.PyFenseMap(self.levelMapName)
         self.add(self.levelMap, z=0)
 
-    def displayEntities(self):
+    def _display_entities(self):
         startTile = self.getPositionFromGrid(self.startTile)
         endTile = self.getPositionFromGrid(self.endTile)
         self.entityMap = entities.PyFenseEntities(self.movePath,
@@ -141,26 +137,26 @@ class PyFenseGame(scene.Scene):
         self.entityMap.push_handlers(self)
         self.add(self.entityMap, z=1)
 
-    def displayHud(self):
+    def _display_hud(self):
         self.hud = hud.PyFenseHud()
         self.hud.push_handlers(self)
         self.add(self.hud, z=2)
 
-    def setGridPix(self, x, y, kind):
+    def set_grid_pix(self, x, y, kind):
         if kind < 0 or kind > 200:
             print("WRONG GRID TYPE, fix ur shit")
             return
         grid_x = int(x / 60)
         grid_y = int(y / 60)
-        self.setGrid(grid_x, grid_y, kind)
+        self.set_grid(grid_x, grid_y, kind)
 
-    def setGrid(self, grid_x, grid_y, kind):
+    def set_grid(self, grid_x, grid_y, kind):
         if kind < 0 or kind > 200:
             print("WRONG GRID TYPE, fix ur shit")
             return
         self.gameGrid[grid_y][grid_x] = kind
 
-    def getGridPix(self, x, y):
+    def get_grid_pix(self, x, y):
         grid_x = int(x / 60)
         grid_y = int(y / 60)
         # gracefully fail for resolution edge cases
@@ -182,7 +178,7 @@ class PyFenseGame(scene.Scene):
         self.hud.updateCurrencyNumber(self.currentCurrency)
 
     def on_user_mouse_motion(self, x, y):
-        self.hud.currentCellStatus = self.getGridPix(x, y)
+        self.hud.currentCellStatus = self.get_grid_pix(x, y)
 
     def on_build_tower(self, towerNumber, pos_x, pos_y):
         toBuildTower = tower.PyFenseTower(towerNumber, (pos_x, pos_y))
@@ -190,7 +186,7 @@ class PyFenseGame(scene.Scene):
             return
         self.currentCurrency -= self.entityMap.build_tower(toBuildTower)
         self.hud.updateCurrencyNumber(self.currentCurrency)
-        self.setGridPix(pos_x, pos_y, int(float("1" + str(towerNumber) +
+        self.set_grid_pix(pos_x, pos_y, int(float("1" + str(towerNumber) +
                         str(toBuildTower.attributes["lvl"]))))
 
     def on_upgrade_tower(self, position):
