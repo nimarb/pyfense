4fbc81b288c09b44234f9f56cb75fab84a1cb8ef
Robin Wachsmann <robin.wachsmann@tum.de>
2015-07-06 15:48:12 +0200

    some test_enemy fixes

--- test/test_enemy.py
+++ test/test_enemy.py
@@ -5,7 +5,7 @@ Created on Tue Jun 16 18:19:41 2015
 @author: Matthias
 """
 import os
-os.chdir(os.path.join('..', 'pyfense'))
+os.chdir('pyfense')
 
 import unittest
 import cocos
@@ -48,8 +48,8 @@ class TestEnemy(unittest.TestCase):
 
     def test_healthmultiplier(self):
         test_path = cocos.actions.MoveBy((0, 0), 0)
-        enemy1 = pyfense_enemy.PyFenseEnemy((0, 0), 0, 1, 1, test_path, 1)
-        enemy2 = pyfense_enemy.PyFenseEnemy((0, 0), 0, 1, 1, test_path, 2.5)
+        enemy1 = enemy.PyFenseEnemy((0, 0), 0, 1, 1, test_path, 1)
+        enemy2 = enemy.PyFenseEnemy((0, 0), 0, 1, 1, test_path, 2.5)
         actualResult = enemy1.attributes["maxhealth"] * 2.5
         result = enemy2.healthPoints
         self.assertEqual(result, actualResult)
