03632208c94434ffa57ba59b598ae07da2710669
Robin Wachsmann <robin.wachsmann@tum.de>
2015-07-07 12:59:32 +0200

    added empty highscore test

---
+++ test/test_highscore.py
@@ -0,0 +1,13 @@
+"""
+Test highscore class.
+"""
+
+import unittest
+
+
+class TestHighscore(unittest.TestCase):
+
+    """test highscore class"""
+
+    def test_handling_charakters(self: )
+    pass
