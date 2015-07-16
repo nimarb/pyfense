265d951d379f49838d69a63616bb44ef42c61366
dan-tum <lechner.daniel@hotmail.de>
2015-07-16 23:05:07 +0200

    Docstring Tower.

--- pyfense/tower.py
+++ pyfense/tower.py
@@ -1,5 +1,5 @@
 """
-pyfense_tower contains PyFenseTower class, inherits from game class
+Create tower that fires projectile.
 """
 
 import cocos
@@ -12,11 +12,22 @@ from pyfense import resources
 class PyFenseTower(sprite.Sprite, pyglet.event.EventDispatcher):
 
     """
-    Needs position in tuple (posx,posy)
-    Takes tower.png found in assets directory
+    Cocos Sprite that handles aiming at a target and firing of projectiles.
     """
 
     def __init__(self, towerNumber, position, level=1):
+        """
+        Create a tower.
+
+        :Parameters:
+            `towerNumber`: int
+                Number of tower.
+            `position` : tuple
+                Position of tower.
+            `level` : int
+                Level of tower.
+        """
+
         self.attributes = resources.tower[towerNumber][level]
         super().__init__(self.attributes["image"], position)
         self.posx = position[0]
@@ -24,12 +35,21 @@ class PyFenseTower(sprite.Sprite, pyglet.event.EventDispatcher):
         self.rotation = 0
         self.target = None
         self._canFire = True
+        # Sound that is played during a shot
         self._shot = resources.shot
         self.schedule(lambda dt: self._fire())
         self.schedule(lambda dt: self._find_next_enemy())
         self.schedule(lambda dt: self._rotate_to_target())
 
     def _fire(self):
+        """
+        Fire projectile with a certain firerate.
+        Scheduled to run every tick and checks whether a target has been
+        aimed at. If yes, it checks whether it can fire based on the firerate
+        (variable _canFire is set to True only if interval since last shot is
+        longer than the firerate)
+        Projectile fired with event on_projectile_fired.
+        """
         if (not self.parent.enemies) or not self.target:
             pass
         elif self._canFire:
@@ -52,21 +72,26 @@ class PyFenseTower(sprite.Sprite, pyglet.event.EventDispatcher):
 
     def _fire_interval(self, dt):
         """
-        Fire the projectile only after firerate interval
+        Fire the projectile only after firerate interval.
         """
+
         self.unschedule(self._fire_interval)
         if not self._canFire:
             self._canFire = True
 
     def _distance(self, a, b):
+        """
+        Calculate distance between two tuples.
+        """
+
         return math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
 
     def _find_next_enemy(self, mode="first"):
         """
-        Find the next enemy (that should be attacked next)
-        either first enemy in range or nearest Enemy
-        standardvalue is first.
+        Find next enemy that should be attacked.
+        Either first enemy in range or nearest enemy. Standardvalue is first.
         """
+
         self.target = None
         self.dist = self.attributes["range"]
         for enemy in self.parent.enemies:
@@ -87,14 +112,21 @@ class PyFenseTower(sprite.Sprite, pyglet.event.EventDispatcher):
                         break
 
     def _rotate_to_target(self):
+        """
+        Rotate tower towards targeted enemy.
+        """
+
         if self.target:
             x = self.target.x - self.x
             y = self.target.y - self.y
-            # should actually be atan2(y, x), but then the angle is wrong
             angle = math.degrees(math.atan2(x, y))
             self.rotation = angle
 
     def get_accumulated_cost(self):
+        """
+        Return cost of tower plus the upgrades that have been bought.
+        """
+
         acc_cost = 0
         level = self.attributes['lvl']
         towerNumber = self.attributes['tower']
